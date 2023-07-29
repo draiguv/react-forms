b073f2b7b5a428d43995708892c1d362e6029a9f
draiguv <dcrauv13@gmail.com>
2023-07-30 01:08:21 +0800

    Learn on change event

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 export default function App() {
   const [headingText, setHeadingText] = useState('Hello');
   const [isMouseOver, setMouseOver] = useState(false);
+  const [name, setName] = useState('');
   
   const handleClick = () => {
     setHeadingText('Submitted');
@@ -15,11 +16,20 @@ export default function App() {
   const handleMouseOut = () => {
     setMouseOver(false);
   };
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
   
   return (
     <div className="container">
-      <h1>{headingText} </h1>
-      <input type="text" placeholder="What's your name?" />
+      <h1>Hello {name !== '' && name + '!'}</h1>
+      {/* <h1>{headingText} </h1> */}
+      <input 
+        onChange={handleChange}
+        type="text" 
+        placeholder="What's your name?" 
+      />
       <button 
         style={ {backgroundColor: isMouseOver ? "black" : "white"} } 
         onClick={handleClick}
