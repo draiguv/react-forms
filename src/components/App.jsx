d36e4323d079f3d130a58542ae5450b627d0876d
draiguv <dcrauv13@gmail.com>
2023-07-30 00:30:02 +0800

    Initial files

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>Hello </h1>
+      <input type="text" placeholder="What's your name?" />
+      <button>Submit</button>
+    </div>
+  );
+}
